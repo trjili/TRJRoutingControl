f8a71a4297aac7c9fa304f1b35f657933f2dbdbd
trjili <trjili@galilee.fr>
2015-02-26 18:15:43 +0100

    Remote rules crud.

---
+++ Controller/RemoteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TRJ\RoutingControlBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RemoteController extends Controller
+{
+    /**
+     * @return Response
+     */
+    public function newAction()
+    {
+
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function updateAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function deleteAction(Request $request)
+    {
+
+    }
+}
